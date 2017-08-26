e09ee32d810483b72b136fbd71ba86af8b430a27
Jonathan Moallem <jdmoallem@gmail.com>
2017-08-26 21:10:09 +1000

    Changed back to SQLite

--- linernotes/settings.py
+++ linernotes/settings.py
@@ -76,12 +76,14 @@ WSGI_APPLICATION = 'linernotes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'linernotes_test_db',
-        'USER': 'linernotes',
-        'PASSWORD': 'linernotes',
-        'HOST': 'localhost',
-        'PORT': '',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'linernotes_test_db',
+        # 'USER': 'linernotes',
+        # 'PASSWORD': 'linernotes',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
