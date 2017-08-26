b24fad96946c3d7559305f540200952e72b8d17e
Jonathan Moallem <jdmoallem@gmail.com>
2017-08-26 21:30:07 +1000

    Allowed more hosts

--- linernotes/settings.py
+++ linernotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5oe=&-249j_9u2qc^1q*^jp818%32i9=1cn4dpgbda84u0xmgf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
